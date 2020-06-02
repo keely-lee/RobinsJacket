2dc57ed158f21ce6e9f6a0b837c599e3c3544002
keely_lee <keely_lee@outlook.com>
2020-06-02 09:52:43 -0400

    Rvert to prev for iCloud issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,9 @@
 #
 class User < ApplicationRecord
   attr_reader :password
-  validates :email, :password_digest, :session_token, :fname, :lname, :funds_available uniqueness: true, presence: true
+  validates :email, :password_digest, :session_token, :fname, :lname, :funds_available, uniqueness: true, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}
-  after_initialize: :ensure_session_token
+  after_initialize :ensure_session_token
 
   has_one :portfolio,
   class_name: :Portfolio,
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/robbins_jacket.jsx',
+  entry: './frontend/robins_jacket.jsx',
   output: {
     path: path.resolve(__dirname, 'app', "assets", "javascripts"),
     filename: 'bundle.js'
