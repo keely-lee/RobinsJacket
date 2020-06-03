97be16942e19d90b92549bd96dbc6ea3d6a9b103
keely_lee <keely_lee@outlook.com>
2020-06-03 02:05:40 -0400

    Create auth route util

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,6 +3,7 @@ import GreetingContainer from './greeting/GreetingContainer';
 import { Route } from 'react-router-dom';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
+import AuthRoute from "./util/route_util"
 
 const App = () => {
   return (
@@ -11,12 +12,11 @@ const App = () => {
         <h3>RobinsJacket, let's get started!</h3>
         <GreetingContainer />
       </header>
-
+      
       <Route path="/login" component={LoginFormContainer} />
       <Route path="/signup" component={SignupFormContainer} />
 
     </div>
-    //routes will go in the div tags
   )
 }
 
--- frontend/robins_jacket.jsx
+++ frontend/robins_jacket.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import configureStore from "./store/store"
-import * as APIUtil from "./util/session_api_util";
+import * as APIUtil from "./util/session_api_util"; //remove
 import Root from "./components/root"; 
-
-import { login, logout, signup } from "./actions/session_actions"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -23,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
   } 
   else { store = configureStore(); }
 
-  window.getState = store.getState;
+  window.getState = store.getState; //remove
 
   ReactDOM.render(<Root store={store}/>, root)
 })
