2b147a9e5ea729d626798916d42610db5f780cc3
keely_lee <keely_lee@outlook.com>
2020-06-09 15:57:00 -0400

    Added majority of splash comp six

--- frontend/components/splash/splash_comp_six.jsx
+++ frontend/components/splash/splash_comp_six.jsx
@@ -7,7 +7,6 @@ class SplashCompSix extends React.Component {
     //EVENTUAL STATE FOR LAST MODAL
   }
 
-
   render() {
     return (
       <div className="splash-comp-six">
@@ -31,14 +30,33 @@ class SplashCompSix extends React.Component {
                 <button type="button" className="splash-six-bottom-link-green">Careers</button>
                 <button type="button" className="splash-six-bottom-link-green">Company News</button>
                 <div className="splash-comp-six-icon-buttons">
-
+                    {/* ICON BUTTONS NEED TO BE ADDED */}
                 </div>
               </section>
             </div>
-            <div className="splash-comp-six-top-right">
 
+            <div className="splash-comp-six-top-right">
+              <section className="splash-comp-six-top-right-top">
+                <h4>RobinSnacks</h4>
+                <p>The 3-minute newsletter with fresh takes on the financial news you need to start your day.</p>
+
+                {/* CHANGE THESE DETAILS */}
+                <form onClick={this.handleSubmit} className="add-email-form" >
+                  {/* add on change eventually for email submit */}
+                  <input type="text" className="add-email-input" placeholder="name@email.com" />
+                  <Link className="button-green access-button" to={""}>Get Access Now</Link>
+                </form>
+              </section>
+
+              <section className="splash-comp-six-right-buttons">
+                <button type="button" className="splash-six-top-link-green-right">Check the background of the firm on FINRA’s Broker Check</button>
+                <button type="button" className="splash-six-top-link-green-right">Robinhood Terms &amp; Conditions</button>
+                <button type="button" className="splash-six-top-link-green-right">Disclosure Library</button>
+                <button type="button" className="splash-six-top-link-green-right">Privacy</button>
+              </section>
             </div>
           </section>
+
           <section className="splash-comp-six-bottom">
             <p>© March 2020 RobinsJacket. All rights reserved.</p>
             <p>RobinsJacket means RobinsMarkets, Inc. and its in-application and web experiences with its family of wholly owned subsidiaries which includes RobinsJacket Financial, RobinsJacket Securities, LLC, RobinsCrypto, and RobinsJacket U.K.</p>
