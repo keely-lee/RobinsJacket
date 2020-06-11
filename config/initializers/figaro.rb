4b24afbf364f11f2b83feef6bd81bcc1c72396a4
keely_lee <keely_lee@outlook.com>
2020-06-11 11:18:31 -0400

    Fix api key files

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,7 +1,7 @@
 # Figaro.require_keys("pusher_app_id", "pusher_key", "pusher_secret")
 
-Pusher.app_id = Figaro.env.pusher_app_id!
-Pusher.key    = Figaro.env.pusher_key!
-Pusher.secret = Figaro.env.pusher_secret!
+# Pusher.app_id = Figaro.env.pusher_app_id!
+# Pusher.key    = Figaro.env.pusher_key!
+# Pusher.secret = Figaro.env.pusher_secret!
 
-
+# Pusher.pusher_API_key = Figaro.env.news_api_key!
