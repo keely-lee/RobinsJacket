43c5e49de46cbd0b20a788a03c3a5db84cd58cbb
keely_lee <keely_lee@outlook.com>
2020-06-11 15:26:15 -0400

    HTTPS routing

--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -2,6 +2,6 @@
 export const receiveNews = () => {
   return $.ajax({
     method: "GET",
-    url: `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=42e27fcfca4d4cf1b71c45c0ed59a278`
+    url: `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=42e27fcfca4d4cf1b71c45c0ed59a278`
   })
 }
