a2589674adcc00b0f08eecb4af9879ac0ce74166
keely_lee <keely_lee@outlook.com>
2020-07-19 21:44:04 -0400

    Prep react hook components

--- frontend/components/user_portfolio/user_portfolio_home_container.jsx
+++ frontend/components/user_portfolio/user_portfolio_home_container.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import UserPortfolioHomeMain from "./user_portfolio_home_main";
 
-const UserPortfolioHome = () => {
-  // const result : any = 
-}
-export default UserPortfolioHome;
+
+// const UserPortfolioHome = () => {
+//   // const result : any = 
+// }
+// export default UserPortfolioHome;
+
+// const mSTP = state
--- frontend/components/user_portfolio/user_portfolio_stock.jsx
+++ frontend/components/user_portfolio/user_portfolio_stock.jsx
@@ -1,14 +1,39 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-function UserPortfolioStock() {
+function UserPortfolioStock(ownProps){
+  // const dispatch = useDispatch();
+  // const ownP = useSelector(ownProps => ownProps)
+  const testState = useSelector(state => state.stocks)
+
+  // console.log(ownP)
+  console.log(testState)
+
   return (
     <div className="stock-comp-main-div">
       <section className="stock-comp-body"></section>
       <section className="stock-comp-trans">
-        <h2>HELLO FROM STOCKS</h2>
+        <div className="stock-comp-options">
+          {/* NEED STOCK NAME FOR H4-TRADE $NAME */}
+          <h4>Trade </h4>
+        </div>
+        <div className="stock-comp-trade-details">
+          <span>Shares</span>
+          <input type="text"/>
+          <span>Market Price</span>
+          <span>PRICE$$</span>
+        </div>
+        <div className="stock-comp-trade-confirm">
+          <span>Estimated COST/CREDIT</span>
+          <span>COST$$</span>
+          <button>BUY/SELL Button</button>
+        </div>
+        <div className="stock-comp-portfolio-details">
+          MONEY$$ BUY/SELL Power Available
+        </div>
       </section>
     </div>
-  )
+  );
 }
 
 export default UserPortfolioStock;
